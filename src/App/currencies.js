5941d883568952983b3cefc888d6015fbfbd08e6
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-09-16 13:17:33 +0200

    created currencies array

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+       short: "EUR",
+       name: "Euro",
+       rate: 4.6307,
+    },
+    {
+       short: "USD",
+       name: "Dolar amerykański",
+       rate: 4.3418,
+    },
+    {
+       short: "GBT",
+       name: "Funt brytyjski",
+       rate: 5.4005
+    },
+ ];
