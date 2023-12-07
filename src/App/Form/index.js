a1000b61c997251986181817bbabd01a59216e72
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-07 11:19:15 +0100

    blocked counting for numbers smaller than 0

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -30,6 +30,7 @@ export const Form = ({ calculateResult, result }) => {
                         placeholder="Wpisz kwotę w złotówkach"
                         type="number"
                         required
+                        min={1}
                     />
                 </label>
             </p>
