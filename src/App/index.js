e7c3d06cb199e7e7a3084a9d0324de1ff41a5189
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-07 11:31:52 +0100

    deleted unnecessary useEffect

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { currencies } from "./currencies";
 import { Form } from "./Form";
 import { Clock } from "./Clock";
