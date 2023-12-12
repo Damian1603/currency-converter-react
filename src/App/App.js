c51bdb9d4a413a4e7f73b4b3cc2d69b2822d20c6
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-12 12:04:24 +0100

    updated

---
+++ src/App/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { currencies } from "./currencies";
+import { Form } from "./Form";
+import { Clock } from "./Clock";
+import "./App.css";
+
+function App() {
+    const [result, setResult] = useState("");
+
+    const calculateResult = (currency, amount) => {
+        const rate = currencies.find(({ short }) => short === currency).rate
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount / rate,
+            currency,
+        })
+    };
+
+    return (
+
+        <div className="app">
+            <div className="data">
+                <Clock />
+            </div>
+            <Form
+                result={result}
+                calculateResult={calculateResult}
+            />
+        </div>
+    );
+};
+
+export default App;
