9dbcad5c806ad15d206d1b3a697d8a12bb35ef59
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-14 14:13:29 +0100

    added custom hook

--- src/App/Clock/useCurrentDate.js
+++ src/App/Clock/useCurrentDate.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export const OnCurrentDate = () => {
+export const useCurrentDate = () => {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
