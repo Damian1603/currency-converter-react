81310168fd391f1610704a95f3a645b5493ab8b7
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-15 15:21:55 +0100

    created styled container

---
+++ src/Container/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+    max-width: 700px;
+    border-radius: 15px;
+    box-shadow: 1px 4px 12px 4px ${({ theme }) => theme.colors.shadowColor};
+    padding: 10px;
+    margin: 20px auto;
+    background-color: ${({ theme }) => theme.colors.backgroundColor};
+    font-family: 'Kanit', sans-serif;;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    margin: 20px
+    }
+`;
