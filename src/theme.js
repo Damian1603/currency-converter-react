badf6caa964b9ec40728eea63318f57d10659b20
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-12-15 15:13:18 +0100

    created theme file

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+    colors: {
+        gray: "#5d5c5c",
+        lightGray: "#d1d1d1",
+        borderButtonGray: "#a19f9f",
+        borderField: "#b4b4b4",
+        buttonActiveGray: "#c4c4c4",
+        backgroundColor: "#c3cecb",
+        shadowColor: "#5a5959",
+        buttonColor: "#d1d1d1"
+    },
+
+    breakpoints: {
+        mobile: (767)
+    }
+};
