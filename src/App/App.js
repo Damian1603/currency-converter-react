88346c3937f55360f1105f9e693cb074fa6adedf
Damian Kuczynski <kuczynski.damian00@wp.pl>
2024-01-07 16:18:54 +0100

    removed result counting function

--- src/App/App.js
+++ src/App/App.js
@@ -1,30 +1,13 @@
-import { useState } from "react";
-import { currencies } from "./currencies";
 import { Form } from "./Form";
 import { Clock } from "./Clock";
 import { StyledContainer } from "../Container/styled";
 
 export function App() {
-    const [result, setResult] = useState("");
-
-    const calculateResult = (currency, amount) => {
-        const rate = currencies.find(({ short }) => short === currency).rate
-
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-        })
-    };
 
     return (
-
         <StyledContainer>
             <Clock />
-            <Form
-                result={result}
-                calculateResult={calculateResult}
-            />
+            <Form />
         </StyledContainer>
     );
 };
