10e666f71f893f6ebc9393a4ce38666a2859cbf8
Damian Kuczynski <kuczynski.damian00@wp.pl>
2024-01-07 16:27:10 +0100

    added status loading and status failure/ moved result calculate function

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import { currencies } from "../currencies";
 import { Result } from "./Result";
 import {
     StyledForm, StyledHeader, StyledContainer,
     StyledLabelText, StyledField, StyledButton,
-    StyledInfo
+    StyledInfo, StyledLoading, StyledFailure
 } from "./styled";
+import { useRatesData } from "./useRatesData";
+import { currencies } from "../currencies";
 
-export const Form = ({ calculateResult, result }) => {
-    const [currency, setCurrency] = useState(currencies[0].short);
+export const Form = () => {
+    const [currency, setCurrency] = useState("EUR");
     const [amount, setAmount] = useState("");
 
     const onSubmit = (event) => {
@@ -16,60 +17,102 @@ export const Form = ({ calculateResult, result }) => {
         calculateResult(currency, amount);
     }
 
+    const [result, setResult] = useState(null);
+    const ratesData = useRatesData();
+
+    const calculateResult = (currency, amount) => {
+        const rate = ratesData.data[currency].value;
+
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: rate * amount,
+            currency,
+        })
+    };
+
+    const findFullName = (currencyShort) => {
+        const foundCurrency = currencies.find(
+            (currency) => currency.shortName === currencyShort
+        );
+        return foundCurrency ? foundCurrency.fullName : "";
+    };
+
     return (
+
         <StyledForm onSubmit={onSubmit}>
             <StyledHeader>
                 Kalkulator walutowy
             </StyledHeader>
-            <p>
-                <StyledContainer>
-                    <StyledLabelText>
-                        *Kwota w PLN:
-                    </StyledLabelText>
-                    <StyledField
-                        as="input"
-                        autoFocus
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        placeholder="Wpisz kwotę w złotówkach"
-                        type="number"
-                        required
-                        min={1}
-                    />
-                </StyledContainer>
-            </p>
-            <p>
-                <StyledContainer>
-                    <StyledLabelText>
-                        Waluta:
-                    </StyledLabelText>
-                    <StyledField
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                    >
-                        {currencies.map((currency => (
-                            <option
-                                key={currency.short}
-                                value={currency.short}
-                            >
-                                {currency.name}
-                            </option>
-                        )))}
-                    </StyledField>
-                </StyledContainer>
-            </p>
-            <p>
-                <StyledButton>
-                    <strong>Przelicz</strong>
-                </StyledButton>
-            </p>
-            <StyledInfo>
-                Kurs pochodzą ze strony nbp.pl z tabeli nr 177/A/NBP/2023 z dnia 2023-09-13
-                <p>
-                    * - to pole musi być uzupełnione
-                </p>
-            </StyledInfo>
-            <Result result={result} />
+            {ratesData.state === "loading"
+                ? (
+                    <StyledLoading>
+                        Ładuję kursy...<br /> To może chwilę potrwać
+                    </StyledLoading>
+                )
+                : (
+                    ratesData.state === "error" ? (
+                        <StyledFailure>
+                            Niestety coś poszło nie tak. Sprawdź połączenie z internetem
+                        </StyledFailure>
+                    ) : (
+                        <>
+                            <p>
+                                <StyledContainer>
+                                    <StyledLabelText>
+                                        *Kwota w PLN:
+                                    </StyledLabelText>
+                                    <StyledField
+                                        as="input"
+                                        autoFocus
+                                        name="amount"
+                                        value={amount}
+                                        onChange={({ target }) => setAmount(target.value)}
+                                        placeholder="Wpisz kwotę w złotówkach"
+                                        type="number"
+                                        required
+                                        min={1}
+                                    />
+                                </StyledContainer>
+                            </p>
+                            <p>
+                                <StyledContainer>
+                                    <StyledLabelText>
+                                        Waluta:
+                                    </StyledLabelText>
+                                    <StyledField
+                                        name="currency"
+                                        value={currency}
+                                        onChange={({ target }) => setCurrency(target.value)}
+                                    >
+                                        {Object.keys(ratesData.data).map((currency) => (
+                                            <option
+                                                key={currency}
+                                                value={currency}
+                                            >
+                                                {currency}&nbsp;{findFullName(currency)}
+                                            </option>
+                                        ))}
+                                    </StyledField>
+                                </StyledContainer>
+                            </p>
+                            <p>
+                                <StyledButton>
+                                    <strong>Przelicz</strong>
+                                </StyledButton>
+                            </p>
+                            <StyledInfo>
+                                Kurs pochodzą ze strony nbp.pl z tabeli nr 177/A/NBP/2023 z dnia 2023-09-13
+                                <p>
+                                    * - to pole musi być uzupełnione
+                                </p>
+                            </StyledInfo>
+                            <Result result={result} />
+                        </>
+                    )
+                )}
         </StyledForm>
+
     );
+
 };
