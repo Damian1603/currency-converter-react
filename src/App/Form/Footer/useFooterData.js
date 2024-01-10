9fb43277d63f16755fca6c3f56180cd1f4fc5634
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-01-10 13:15:50 +0100

    formated current date

---
+++ src/App/Form/Footer/useFooterData.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+const formatDate = {
+
+    day: "numeric",
+    month: "numeric",
+    year: "numeric"
+
+}
+
+export const useFooterData = (ratesData) => {
+
+    const [ratesDay, setRatesDay] = useState(new Date());
+
+    useEffect(() => {
+        if (ratesData.state === "success") {
+            setRatesDay(new Date(ratesData.date))
+        }
+
+    }, [ratesData])
+
+    const formattedRatesDay = ratesDay.toLocaleDateString(undefined, formatDate);
+
+    return formattedRatesDay;
+};
