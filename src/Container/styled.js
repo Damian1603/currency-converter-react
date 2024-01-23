16281bd22d7af4a7f8d6329e450350f25c83d23e
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-01-23 08:38:53 +0100

    updated margin container

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -5,7 +5,7 @@ export const StyledContainer = styled.div`
     border-radius: 15px;
     box-shadow: 1px 4px 12px 4px ${({ theme }) => theme.colors.shadowColor};
     padding: 10px;
-    margin: 20px auto;
+    margin: 100px auto;
     background-color: ${({ theme }) => theme.colors.backgroundColor};
     font-family: 'Kanit', sans-serif;;
 
