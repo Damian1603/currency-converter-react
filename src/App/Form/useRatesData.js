a20280b6a82d62b11ecdc7954b7f333155348ea9
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-02-06 10:00:34 +0100

    I moved the fetching rates and parsing to the json object to a separate function

--- src/App/Form/useRatesData.js
+++ src/App/Form/useRatesData.js
@@ -1,23 +1,18 @@
 import { useState, useEffect } from 'react';
+import { fetchRatesData } from '../fetchRatesData';
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState({
         state: "loading",
     });
 
-    const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_x7ObLVw9onFaGGeacM2AM12N6Eo14dHx6Kyl4BI5&currencies=&base_currency=PLN"
 
     useEffect(() => {
         const fetchRates = async () => {
             try {
-                const response = await fetch(API_URL)
-
-                if (!response.ok) {
-                    throw new Error(response.statusText)
-                }
-
-                const { meta, data } = await response.json();
-
+                
+                const { meta, data } = await fetchRatesData();
+                
                 setRatesData({
                     state: "success",
                     meta,
