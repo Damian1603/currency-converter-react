8e3407f7a369e1a213f8dfb9167a6c237676f3c3
Damian Kuczyński <kuczynski.damian00@wp.pl>
2024-04-23 10:43:38 +0200

    modified fetching rates function on axios

--- src/App/fetchRatesData.js
+++ src/App/fetchRatesData.js
@@ -1,10 +1,12 @@
+import axios from "axios";
+
 const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_x7ObLVw9onFaGGeacM2AM12N6Eo14dHx6Kyl4BI5&currencies=&base_currency=PLN"
 
 export const fetchRatesData = async () => {
-    const response = await fetch(API_URL)
-    if (!response.ok) {
+    const response = await axios.get(API_URL)
+    if (response.status !== 200) {
         throw new Error(response.statusText)
     }
-    const data = await response.json();
+    const data = await response.data;
     return data;
 };
